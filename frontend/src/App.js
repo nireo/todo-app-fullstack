89f15642f7679cd5be5d825ffafa823ed8f61901
nireo <eemil.lehtikangas1@gmail.com>
2019-05-28 16:24:31 +0300

    added input field and few states

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,28 @@ import './App.css';
 import TodoItem from "./components/todoitem"
 
 const App = () => {
+    const [items, setItems] = useState([])
+    const [newItem, setNewItem] = useState('')
+
+    const handleItem = (event) => {
+        setNewItem(event.target.value)
+    }
+
     return (
         <div className="container">
             <div className="my-3 p-3 bg-white rounded shadow-sm">
-                <h6 className="border-bottom border-gray pb-2 mb-0">Todo list</h6>
+                <h4 className="border-bottom border-gray pb-2 mb-0">Todo list</h4>
+                <div className="media text-muted pt-3">
+                    <form className="form-inline media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                        <h6>New Item</h6>
+                        <div className="form-group mx-sm-3 mb-2">
+                            <label htmlFor="inputPassword2" className="sr-only">Password</label>
+                            <input className="form-control" />
+                        </div>
+                        <button type="submit" className="btn btn-primary mb-2">Add new</button>
+                    </form>
+                </div>
+
                 <div className="media text-muted pt-3">
                     <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
                         Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
@@ -26,7 +44,7 @@ const App = () => {
                     </p>
                 </div>
                 <small className="d-block text-left mt-3">
-                    <a href="#">Github</a>
+                    <a href="https://github.com/nireo/todo-app-fullstack" target="_blank" rel="noopener noreferrer">Github</a>
                 </small>
             </div>
         </div>
