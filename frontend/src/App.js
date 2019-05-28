03e4ecc955790fb1bb3ee69671037441c5bcd235
nireo <eemil.lehtikangas1@gmail.com>
2019-05-28 15:32:01 +0300

    added todoitem.js and came up with the general style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import TodoItem from "./components/todoitem"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="container">
+            <div className="my-3 p-3 bg-white rounded shadow-sm">
+                <h6 className="border-bottom border-gray pb-2 mb-0">Todo list</h6>
+                <div className="media text-muted pt-3">
+                    <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                        Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
+                        tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                    </p>
+                </div>
+                <div className="media text-muted pt-3">
+                    <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                        Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
+                        tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                    </p>
+                </div>
+                <div className="media text-muted pt-3">
+                    <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                        Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
+                        tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                    </p>
+                </div>
+                <small className="d-block text-left mt-3">
+                    <a href="#">Github</a>
+                </small>
+            </div>
+        </div>
+    )
 }
 
 export default App;
---
+++ frontend/src/components/todoitem.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const TodoItem = (props) => {
+
+}
+
+export default TodoItem
