1010bf738ba570da34ebfe41aed2ddc5f25aad50
nireo <eemil.lehtikangas1@gmail.com>
2019-05-29 17:54:41 +0300

    added an update and a remove function to frontend

--- frontend/src/services/item.js
+++ frontend/src/services/item.js
@@ -1,5 +1,5 @@
 import axios from "axios/index"
-const baseUrl = "http://localhost:3001/items"
+const baseUrl = "http://localhost:3001"
 
 const getAll = () => {
     const request = axios.get(baseUrl)
@@ -11,6 +11,14 @@ const create = newObject => {
     return request.then(response => response.data)
 }
 
+const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+}
 
+const remove = id => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response => response.data)
+}
 
-export default {getAll}
+export default {getAll, create}
