a32b4bd14b002c84d8c88f78f23bc182aa08125a
nireo <eemil.lehtikangas1@gmail.com>
2019-05-29 18:37:29 +0300

    restyled buttons

--- frontend/src/components/todoitem.js
+++ frontend/src/components/todoitem.js
@@ -1,12 +1,21 @@
 import React from "react"
 
+/*
+<button className="btn btn-primary mb-2 border-bottom border-gray" onClick={() => props.removeItem(item.id)}>delete</button>
+<p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+{item.name}
+</p>
+<p className="pb-3 mb-0 small border-bottom border-gray">Added: {item.itemTime}</p>
+*/
+
 const TodoItem = (props) => {
     const allItems = props.items.map(item => <div className="media text-muted pt-3">
-        <button className=" pb-3 mb-0 small lh-125 border-bottom border-gray" onClick={() => props.removeItem(item.id)}>delete</button>
+        <p className="pb-2 mb-0 small border-bottom border-gray" ><button className="btn btn-primary pb-0 mb-0 btn-sm " onClick={() => props.removeItem(item.id)}>delete</button></p>
         <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-            {item.name}
+        {item.name}
         </p>
         <p className="pb-3 mb-0 small border-bottom border-gray">Added: {item.itemTime}</p>
+
     </div>)
     return (
         <div>{allItems}</div>
@@ -14,4 +23,5 @@ const TodoItem = (props) => {
 }
 
 export default TodoItem
+
 
